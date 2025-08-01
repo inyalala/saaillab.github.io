504bd8178b86df8eabb0c2ee835f36bcb5878566
Innocent Nyalala <11386133+inyalala@users.noreply.github.com>
2025-08-01 19:29:38 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// Configuration for inyalala.github.io/saaillab.github.io deployment
+// Configuration for saaillab.github.io deployment
 export default defineConfig({
-  site: 'https://inyalala.github.io',
-  base: '/saaillab.github.io',
+  site: 'https://saaillab.github.io',
 });
